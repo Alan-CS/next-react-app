709beb63855be0a29b74e304ff73b4422937cee4
AlanCS <AlanCS1010@gmail.com>
2024-06-13 16:34:31 -0700

    Added the React Docs Challenge exercise.

--- app/snippets/page.tsx
+++ app/snippets/page.tsx
@@ -2,6 +2,7 @@
 
 import {TWBeforeAfter} from "@/app/snippets/tw-before-after";
 import Scoreboard from "@/app/snippets/updating-objects-in-state";
+import Canvas from "./drag-box"
 import Link from "next/link";
 
 export default async function Page() {
@@ -15,16 +16,26 @@ export default async function Page() {
                     </Link>
                 </div>
             </div>
+
             <div className="p-2">
                 <h1 className="text-2xl text-center mb-4">Usage of before and after in TW</h1>
                 <TWBeforeAfter/>
             </div>
+
             <div className="p-2 mt-4 border-t-2 border-t-red-700">
                 <h1 className="text-2xl text-center mb-4">Update Objects In State</h1>
                 <div>
                     <Scoreboard/>
                 </div>
             </div>
+
+            <div className="p-2 mt-4 border-t-2 border-t-red-700">
+                <h1 className="text-2xl text-center mb-4">React Docs Update Objects In State</h1>
+                <div className="relative">
+                    <Canvas/>
+                </div>
+            </div>
+
         </main>
     );
 }
