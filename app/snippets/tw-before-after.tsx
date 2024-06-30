e4ece45b8383de962112d3ce10fa1c072ff37d6c
AlanCS <AlanCS1010@gmail.com>
2024-06-30 07:56:33 -0700

    Fixed ESLint error regarding quote.

--- app/snippets/tw-before-after.tsx
+++ app/snippets/tw-before-after.tsx
@@ -10,7 +10,7 @@ export function TWBeforeAfter() {
                 className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative mx-2">
                       <span className="relative text-white">annoyed</span>
                   </span>
-            all the time, people think that you're busy.
+            all the time, people think that you&apos;re busy.
         </blockquote>
 
         {/*The below is done without before and after using a real html element*/}
@@ -21,7 +21,7 @@ export function TWBeforeAfter() {
                     <span className="block absolute -inset-1 -skew-y-3 bg-pink-500" aria-hidden="true"></span>
                     <span className="relative text-white">annoyed</span>
                   </span>
-            all the time, people think that you're busy.
+            all the time, people think that you&apos;re busy.
         </blockquote>
 
         </>
