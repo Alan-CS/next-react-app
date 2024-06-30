fee58ca50dba19e871ebc4c9e6bca4a8aecc0817
AlanCS <AlanCS1010@gmail.com>
2024-06-30 08:59:53 -0700

    Added typescript bindings and made other enhancements.

--- app/snippets/DebouncedButtons.tsx
+++ app/snippets/DebouncedButtons.tsx
@@ -1,22 +1,32 @@
 // ALAN: Taken from https://react.dev/learn/referencing-values-with-refs#fix-debouncing
 
 // let timeoutID; //This will not work since external id will be shared by all buttons
-import {useRef} from 'react';
+import { useRef, ReactNode, MouseEvent } from 'react';
 
-function DebouncedButton({ onClick, children }) {
-    const timeoutRef = useRef(null);
+interface DebouncedButtonProps {
+    onClick: () => void;
+    children: ReactNode;
+}
+
+function DebouncedButton({ onClick, children }: DebouncedButtonProps) {
+    const timeoutRef = useRef<number | null>(null);
+
+    function handleClick(event: MouseEvent<HTMLButtonElement>) {
+        event.preventDefault(); // Prevent default action if needed
+
+        // clearTimeout(timeoutID); // Will not work
+        // timeoutID = setTimeout(() => { // Will not work
+        if (timeoutRef.current !== null) {
+            clearTimeout(timeoutRef.current);
+        }
+
+        timeoutRef.current = window.setTimeout(() => {
+            onClick();
+        }, 1000);
+    }
 
     return (
-        <button className="btn-primary-block mb-2" onClick={() => {
-
-            // clearTimeout(timeoutID); // Will not work
-            // timeoutID = setTimeout(() => { // Will not work
-
-            clearTimeout(timeoutRef.current);
-            timeoutRef.current = setTimeout(() => {
-                onClick();
-            }, 1000);
-        }}>
+        <button className="btn-primary-block mb-2" onClick={handleClick}>
             {children}
         </button>
     );
@@ -25,21 +35,15 @@ function DebouncedButton({ onClick, children }) {
 export default function DebouncedButtons() {
     return (
         <>
-            <DebouncedButton
-                onClick={() => alert('Spaceship launched!')}
-            >
+            <DebouncedButton onClick={() => alert('Spaceship launched!')}>
                 Launch the spaceship
             </DebouncedButton>
-            <DebouncedButton
-                onClick={() => alert('Soup boiled!')}
-            >
+            <DebouncedButton onClick={() => alert('Soup boiled!')}>
                 Boil the soup
             </DebouncedButton>
-            <DebouncedButton
-                onClick={() => alert('Lullaby sung!')}
-            >
+            <DebouncedButton onClick={() => alert('Lullaby sung!')}>
                 Sing a lullaby
             </DebouncedButton>
         </>
-    )
+    );
 }
